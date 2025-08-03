4524c72710386b40b23050871c6ab977bbad1982
Lucas Siqueira Santos <lucas.dev.ssantos@gmail.com>
2025-08-03 08:45:27 -0300

    Roubando código exemplo do site da GTK e att o .json q foi esquecido de dar commit

--- src/main.js
+++ src/main.js
@@ -1,19 +1,32 @@
 #!/usr/bin/gjs
 
-const { Gtk } = imports.gi;
+const GIRepository = imports.gi.GIRepository;
+const repo = GIRepository.Repository.get_default();
 
-Gtk.init();
+let gtkVersion;
+if(repo.enumerate_versions('Gtk').includes('4.0'))
+{
+    gtkVersion = '4.0';
+}
+else if(repo.enumerate_versions('Gtk').includes('3.0'))
+{
+    gtkVersion = '3.0';
+}
 
-const win = new Gtk.Window({
-    title: "Olá Flatpak!",
-    default_width: 300,
-    default_height: 100,
+imports.gi.versions.Gtk = gtkVersion;
+const Gtk = imports.gi.Gtk;
+
+// Create a new application
+let app = new Gtk.Application({ application_id: 'org.lucasssantos.GTKTest' });
+
+// When the application is launched…
+app.connect('activate', () => {
+    // … create a new window …
+    let win = new Gtk.ApplicationWindow({ application: app });
+    // … with a button in it …
+    let btn = new Gtk.Button({ label: 'Hello, World!' });
+    // … which closes the window when clicked
+    btn.connect('clicked', () => { win.close(); });
+    win.set_child(btn);
+    win.present();
 });
-
-win.connect("destroy", () => Gtk.main_quit());
-
-const label = new Gtk.Label({ label: "Hello, mundo com GTK + JS!" });
-win.set_child(label);
-
-win.show();
-Gtk.main();
